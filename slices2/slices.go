dab04b5d891842068c99d4f2dbef9a6b274a61d9
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2023-06-03 13:10:29 -0400

    slices2: FoldLeft, FoldRight, DedupSorted, DedupSortedFunc

--- slices2/slices.go
+++ slices2/slices.go
@@ -19,3 +19,52 @@ func Filter[T any, S ~[]T](xs S, fn func(T) bool) S {
 	}
 	return ret
 }
+
+// FoldLeft implements foldl, as described here
+// https://en.wikipedia.org/wiki/Fold_(higher-order_function)
+func FoldLeft[X, Acc any, S ~[]X](xs S, init Acc, fn func(Acc, X) Acc) Acc {
+	acc := init
+	for i := range xs {
+		acc = fn(acc, xs[i])
+	}
+	return acc
+}
+
+// FoldRight implements foldr, as described here
+// https://en.wikipedia.org/wiki/Fold_(higher-order_function)
+func FoldRight[X, Acc any, S ~[]X](xs S, init Acc, fn func(Acc, X) Acc) Acc {
+	acc := init
+	for i := len(xs) - 1; i >= 0; i-- {
+		acc = fn(acc, xs[i])
+	}
+	return acc
+}
+
+// DedupSorted removes duplicate items according to eq
+// It doesn't actually matter how the items are sorted as long as items which could be the same are adjacent.
+func DedupSorted[T comparable, S ~[]T](xs S) S {
+	var deleted int
+	for i := range xs {
+		if i > 0 && xs[i] == xs[i-1] {
+			deleted++
+		} else {
+			xs[i-deleted] = xs[i]
+		}
+	}
+	return xs[:len(xs)-deleted]
+}
+
+// DedupSortedFunc removes duplicate items according to eq
+// It doesn't actually matter how the items are sorted as long as items which could be the same are adjacent.
+// Note that the last arg `eq` is a function that returns true for equality, not for less-than.
+func DedupSortedFunc[T any, S ~[]T](xs S, eq func(a, b T) bool) S {
+	var deleted int
+	for i := range xs {
+		if i > 0 && eq(xs[i], xs[i-1]) {
+			deleted++
+		} else {
+			xs[i-deleted] = xs[i]
+		}
+	}
+	return xs[:len(xs)-deleted]
+}
